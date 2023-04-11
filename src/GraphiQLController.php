88446b4d9c51fe9f2a5e4e295e4bc47331520a85
Abner Tudtud <abner@enjin.io>
2023-04-11 15:39:11 +0800

    Old version graphiql

--- src/GraphiQLController.php
+++ src/GraphiQLController.php
@@ -2,10 +2,42 @@
 
 namespace MLL\GraphiQL;
 
+use Illuminate\Config\Repository;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Support\Str;
+
 class GraphiQLController
 {
-    public function __invoke()
+    public function __invoke(Request $request, Repository $config): View
     {
-        return view('graphiql::index');
+        $routePrefix = $config->get('graphql.graphiql.prefix', 'graphiql');
+        $schemaName = $this->findSchemaNameInRequest($request, "/$routePrefix");
+
+        $graphqlPath = '/' . $config->get('graphql.route.prefix', 'graphql');
+
+        if ($schemaName) {
+            $graphqlPath .= '/' . $schemaName;
+        }
+        $graphqlPath = '/' . trim($graphqlPath, '/');
+        return view('graphiql::graphiql', [
+            'graphqlPath' => $graphqlPath,
+            'schema' => $schemaName,
+        ]);
     }
+    protected function findSchemaNameInRequest(Request $request, string $routePrefix): ?string
+    {
+        $path = $request->getPathInfo();
+
+        if (!Str::startsWith($path, $routePrefix)) {
+            return null;
+        }
+
+        return trim(Str::after($path, $routePrefix), '/');
+    }
+
+    // public function __invoke()
+    // {
+    //     return view('graphiql::index');
+    // }
 }
